c8fc4ecec312c61de235de57fb66280ceb6da6ac
Tiago Vaz <tiago.vaz@hotmail.com>
2020-07-29 14:46:58 -0300

    Add TemplateView to the project for mvc approach. Refactoring tests

--- nice-locations/src/app.controller.spec.ts
+++ nice-locations/src/app.controller.spec.ts
@@ -16,7 +16,14 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(appController.listLocations()).toMatchObject({
+        locations: [
+          'Location 1',
+          'Location 2',
+          'Location 3',
+          'Location 4',
+        ]
+      });
     });
   });
 });
---
+++ nice-locations/test/test-setup.ts
@@ -0,0 +1,5 @@
+import addJestHbsExtension from 'jest-hbs-extension';
+import { resolve } from 'path';
+
+const VIEWS_PATH = resolve(__dirname, '..', 'views');
+addJestHbsExtension(VIEWS_PATH);
